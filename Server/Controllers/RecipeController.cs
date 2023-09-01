83c92d2ee7ff66c64dfa5a739bea01f54fc85dba
OakeseskAo <oakes.a@live.com>
2023-08-31 22:18:10 -0400

    Frontend work
    Setup meal idea view and scaffold out web api

---
+++ Server/Controllers/RecipeController.cs
@@ -0,0 +1,21 @@
+﻿using AiChef.Server.Data;
+using AIChef.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIChef.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        [HttpPost, Route("GetRecipeIdeas")]
+        public async Task<ActionResult<List<Idea>>> GetRecipeIdeas(RecipeParms recipeParms)
+        {
+            return SampleData.RecipeIdeas;
+        }
+
+
+
+    }
+}
