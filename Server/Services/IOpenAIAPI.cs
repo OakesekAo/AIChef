bffe2815e47d406482eb3b392dc92f6f9a919eee
OakeseskAo <oakes.a@live.com>
2023-09-07 23:28:38 -0400

    openAI service work
    added start of the interface, service and api function calls

---
+++ Server/Services/IOpenAIAPI.cs
@@ -0,0 +1,10 @@
+﻿using AIChef.Shared;
+
+namespace AIChef.Server.Services
+{
+    public interface IOpenAIAPI
+    {
+        Task<List<Idea>> CreateRecipeideas(string mealtime, List<string> ingredients);
+
+    }
+}
